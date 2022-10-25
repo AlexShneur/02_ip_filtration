9025a8448dcb77c72d4203f1f47e53b57d7f3050
AlexShneur <alexsh93@live.ru>
2022-10-25 16:26:42 +0300

    Add files via upload
    1. Changed vector<vector<string>> to vector<array<int,N>> to hold ip-addresses pool.
    2. Added alies my_ip_array for vector<array<int,N>>.
    3. Added memory reservation for vectors.
    4. Changed push_back to emplace_back and added move-semantic.
    5. Added header file 02_ip_filtration.h
    6. Changed CMakeLists.txt.

--- 02_ip_filtration.cpp
+++ 02_ip_filtration.cpp
@@ -1,12 +1,12 @@
-﻿#include <cassert>
-#include <cstdlib>
+﻿#include "02_ip_filtration.h"
+
 #include <iostream>
-#include <string>
-#include <vector>
 #include <fstream>
 #include <algorithm>
+#include <cassert>
+#include <cstdlib>
 
-using namespace std;
+constexpr int POSSIBLE_LINE_COUNT = 1000;       //default line count in the file ip_filter.tsv. May be any positive number
 
 // ("",  '.') -> [""]
 // ("11", '.') -> ["11"]
@@ -33,19 +33,19 @@ vector<string> split(const string& str, char d)
     return r;
 }
 
-bool operator < (const vector<string>& lhs, const vector<string>& rhs)
+bool operator < (const array<int, N>& lhs, const array<int, N>& rhs)
 {
-    for (auto i = 0; i < 4; ++i)
+    for (auto i = 0; i < N; ++i)
     {
-        auto lnum = stoi(lhs[i]);
-        auto rnum = stoi(rhs[i]);
+        auto lnum = lhs[i];
+        auto rnum = rhs[i];
         if (lnum != rnum)
             return lnum < rnum;
     }
     return false;
 }
 
-void print(const vector<vector<string>>& ip_data)
+void print(const my_ip_array& ip_data)
 {
     for (auto ip = ip_data.cbegin(); ip != ip_data.cend(); ++ip)
     {
@@ -61,38 +61,41 @@ void print(const vector<vector<string>>& ip_data)
     }
 }
 
-vector<vector<string>> filter(const vector<vector<string>>& ip_pool, const int& val)
+my_ip_array filter(const my_ip_array& ip_pool, const int& val)
 {
-    vector<vector<string>> result;
+    my_ip_array result;
+    result.reserve(ip_pool.size());
     for (auto ip = ip_pool.cbegin(); ip != ip_pool.cend(); ++ip)
     {
-        if ((*ip).at(0) == to_string(val))
+        if ((*ip).at(0) == val)
             result.emplace_back(*ip);
     }
     return result;
 }
 
-vector<vector<string>> filter(const vector<vector<string>>& ip_pool, const int& val1, const int& val2)
+my_ip_array filter(const my_ip_array& ip_pool, const int& val1, const int& val2)
 {
-    vector<vector<string>> result;
+    my_ip_array result;
+    result.reserve(ip_pool.size());
     for (auto ip = ip_pool.cbegin(); ip != ip_pool.cend(); ++ip)
     {
-        if ((*ip).at(0) == to_string(val1) &&
-            (*ip).at(1) == to_string(val2))
+        if ((*ip).at(0) == val1 &&
+            (*ip).at(1) == val2)
             result.emplace_back(*ip);
     }
     return result;
 }
 
-vector<vector<string>> filter_any(const vector<vector<string>>& ip_pool, const int& val)
+my_ip_array filter_any(const my_ip_array& ip_pool, const int& val)
 {
-    vector<vector<string>> result;
+    my_ip_array result;
+    result.reserve(ip_pool.size());
     for (auto ip = ip_pool.cbegin(); ip != ip_pool.cend(); ++ip)
     {
         bool needOutput = false;
         for (auto ip_part = ip->cbegin(); ip_part != ip->cend(); ++ip_part)
         {
-            if (*ip_part == to_string(val))
+            if (*ip_part == val)
             {
                 needOutput = true;
                 break;
@@ -109,17 +112,26 @@ int main(int argc, char const* argv[])
     try
     {
         ifstream ip_file("ip_filter.tsv");
+        my_ip_array ip_pool;
+        ip_pool.reserve(POSSIBLE_LINE_COUNT);
+
         string line;
-        vector<vector<string>> ip_pool;
         while (getline(ip_file, line))
         {
             auto v = split(line, '\t');
-            ip_pool.push_back(split(v.at(0), '.'));
+            auto ip = (split(v.at(0), '.'));
+
+            array<int, N> ip_arr;
+            for (auto i = 0; i < N; i++)
+            {
+                ip_arr[i] = stoi(ip.at(i));
+            }
+            ip_pool.emplace_back(move(ip_arr));
         }
 
         // TODO reverse lexicographically sort
         sort(ip_pool.rbegin(), ip_pool.rend(),
-            [](const vector<string>& x, const vector<string>& y)
+            [](const array<int, N>& x, const array<int, N>& y)
             {
                 return x < y;
             });
